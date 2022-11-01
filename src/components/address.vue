afc0730559ccc2abfc8be7bf174b75d931d555bd
zeyuanHong <hongzeyuan0@gmail.com>
2022-11-01 16:41:34 +0800

    修改了一些格式

--- src/components/address.vue
+++ src/components/address.vue
@@ -1,117 +1,143 @@
 <template>
-    <van-nav-bar title="收货地址" left-arrow @click-left="hideAddress('cart')" fixed placeholder />
-    <van-address-list v-model="chosenAddressId" default-tag-text="默认" :list="address" @add="onAdd" @edit="onEdit"
-        @select="onSelect" />
-    <van-popup v-model:show="show">
-        <van-address-edit :address-info="initInfo" :area-list="areaList" show-postal :show-delete="isDelete"
-            show-set-default :search-result="searchResult" :area-columns-placeholder="['请选择', '请选择', '请选择']"
-            @save="onSave" @delete="onDelete" />
-    </van-popup>
+  <van-nav-bar
+    title="收货地址"
+    left-arrow
+    @click-left="hideAddress('cart')"
+    fixed
+    placeholder
+  />
+  <van-address-list
+    v-model="chosenAddressId"
+    default-tag-text="默认"
+    :list="address"
+    @add="onAdd"
+    @edit="onEdit"
+    @select="onSelect"
+  />
+  <van-popup v-model:show="show">
+    <van-address-edit
+      :address-info="initInfo"
+      :area-list="areaList"
+      show-postal
+      :show-delete="isDelete"
+      show-set-default
+      :search-result="searchResult"
+      :area-columns-placeholder="['请选择', '请选择', '请选择']"
+      @save="onSave"
+      @delete="onDelete"
+    />
+  </van-popup>
 </template>
 
 <script>
-import { areaList } from '@vant/area-data'
-import { v4 as uuidv4 } from 'uuid'
-import { mapGetters, mapActions } from 'vuex'
+import { areaList } from "@vant/area-data";
+import { v4 as uuidv4 } from "uuid";
+import { mapGetters, mapActions } from "vuex";
 export default {
-    name: 'Address',
-    props: ['handleAddress', 'hideAddress'],
-    data() {
-        return {
-            areaList, // 全国省市区数据
-            chosenAddressId: "",
-            show: false, // 显示地址编辑弹窗
-            isDelete: false, // 是否显示删除按钮
-            initInfo: {}, // 地址信息初始值
-        }
-    },
+  name: "Address",
+  props: ["handleAddress", "hideAddress"],
+  data() {
+    return {
+      areaList, // 全国省市区数据
+      chosenAddressId: "",
+      show: false, // 显示地址编辑弹窗
+      isDelete: false, // 是否显示删除按钮
+      initInfo: {}, // 地址信息初始值
+    };
+  },
 
-    mounted() {
-        // 第一次进入购物车收货地址应该是显示默认收货地址
-        for (let i = 0; i < this.address.length; i++) {
-            if (this.address[i].isDefault) { // 找到默认地址并传递到父组件
-                this.handleAddress(this.address[i]);
-                this.chosenAddressId = this.address[i].id;
-                break;
-            }
-        }
-    },
-
-    computed: {
-        ...mapGetters('user', ['address'])
-    },
-
-    methods: {
-        ...mapActions('user', ['setProfileAddressAction']),
-
-        onAdd() { // 显示添加用户地址
-            this.show = true;
-            this.isDelete = false;
-            this.initInfo = {};// 添加添加初始数据为空对象
-        },
-        onEdit(content) {// 弹窗编辑用户地址，content是点击当前的地址信息。
-            this.initInfo = content; // 点击编辑，把点击的地址数据填充到填写框
-            this.show = true;
-            this.isDelete = true;
-        },
-
-        onSave(content) { // 点击保存地址，content是地址数据
-            // 保存的时候需要根据initInfo数据是否有值，判断是新增地址还是修改地址。
-            let data = {
-                ...content,
-                address: `${content.province} ${content.city} ${content.county} ${content.addressDetail}`
-            };
-            if (this.initInfo.id) { // 修改数据
-                for (let i = 0; i < this.address.length; i++) {
-                    if (this.address[i].id === this.initInfo.id) {
-                        this.address[i] = { ...this.address[i], ...data }
-                        break;
-                    }
-                }
-            } else { // 新增数据
-                this.address.push({
-                    ...data,
-                    id: uuidv4(), // 生成唯一id
-                });
-            }
-            // 默认收货地址只能设置一个，如果新加或者修改的地址设置了默认地址，需求吧其他默认地址给隐藏.
-            if (content.isDefault) { // 设置了默认地址
-                this.address.forEach(function (item) {
-                    if (item.id !== content.id) {
-                        item.isDefault = false;
-                    }
-                });
-            }
-            // 更新全局状态和后端数据库的地址数据。
-            this.setProfileAddressAction({ data: this.address })
-                .then(() => {
-                    this.show = false;// 应该是后端保存成功才隐藏弹窗
-                })
-        },
-
-        onDelete() { // 删除地址
-            for (let i = 0; i < this.address.length; i++) {
-                if (this.address[i].id === this.initInfo.id) {
-                    this.address.splice(i, 1);
-                    break;
-                }
-            }
-            this.setProfileAddressAction({ data: this.address })
-                .then(() => {
-                    this.show = false;// 应该是后端保存成功才隐藏弹窗
-                })
-        },
-
-        onSelect(content) {
-            this.handleAddress(content);// 把当前选中的地址传到父组件的handleAddress方法。
-        }
+  mounted() {
+    // 第一次进入购物车收货地址应该是显示默认收货地址
+    for (let i = 0; i < this.address.length; i++) {
+      if (this.address[i].isDefault) {
+        // 找到默认地址并传递到父组件
+        this.handleAddress(this.address[i]);
+        this.chosenAddressId = this.address[i].id;
+        break;
+      }
     }
-}
+  },
+
+  computed: {
+    ...mapGetters("user", ["address"]),
+  },
+
+  methods: {
+    ...mapActions("user", ["setProfileAddressAction"]),
+
+    onAdd() {
+      // 显示添加用户地址
+      this.show = true;
+      this.isDelete = false;
+      this.initInfo = {}; // 添加添加初始数据为空对象
+    },
+    onEdit(content) {
+      // 弹窗编辑用户地址，content是点击当前的地址信息。
+      this.initInfo = content; // 点击编辑，把点击的地址数据填充到填写框
+      this.show = true;
+      this.isDelete = true;
+    },
+
+    onSave(content) {
+      // 点击保存地址，content是地址数据
+      // 保存的时候需要根据initInfo数据是否有值，判断是新增地址还是修改地址。
+      let data = {
+        ...content,
+        address: `${content.province} ${content.city} ${content.county} ${content.addressDetail}`,
+      };
+      if (this.initInfo.id) {
+        // 修改数据
+        for (let i = 0; i < this.address.length; i++) {
+          if (this.address[i].id === this.initInfo.id) {
+            this.address[i] = { ...this.address[i], ...data };
+            break;
+          }
+        }
+      } else {
+        // 新增数据
+        this.address.push({
+          ...data,
+          id: uuidv4(), // 生成唯一id
+        });
+      }
+      // 默认收货地址只能设置一个，如果新加或者修改的地址设置了默认地址，需求吧其他默认地址给隐藏.
+      if (content.isDefault) {
+        // 设置了默认地址
+        this.address.forEach(function (item) {
+          if (item.id !== content.id) {
+            item.isDefault = false;
+          }
+        });
+      }
+      // 更新全局状态和后端数据库的地址数据。
+      this.setProfileAddressAction({ data: this.address }).then(() => {
+        this.show = false; // 应该是后端保存成功才隐藏弹窗
+      });
+    },
+
+    onDelete() {
+      // 删除地址
+      for (let i = 0; i < this.address.length; i++) {
+        if (this.address[i].id === this.initInfo.id) {
+          this.address.splice(i, 1);
+          break;
+        }
+      }
+      this.setProfileAddressAction({ data: this.address }).then(() => {
+        this.show = false; // 应该是后端保存成功才隐藏弹窗
+      });
+    },
+
+    onSelect(content) {
+      this.handleAddress(content); // 把当前选中的地址传到父组件的handleAddress方法。
+    },
+  },
+};
 </script>
 
 <style lang="scss" scoped>
 // 给顶部收货地址设置固定高度
 .van-nav-bar__placeholder {
-    height: 46px;
-  }
+  height: 46px;
+}
 </style>
--- src/pages/Cart/index.vue
+++ src/pages/Cart/index.vue
@@ -1,22 +1,45 @@
 <template>
   <!-- 若地址展示，则购物车卡片不展示 -->
   <div v-show="showCom === 'cart'">
-    <van-nav-bar :title="`购物车(${cartNum})`" :left-arrow="showBack === 'detail'" @click-left="$router.go(-1)" fixed
-      placeholder />
+    <van-nav-bar
+      :title="`购物车(${cartNum})`"
+      :left-arrow="showBack === 'detail'"
+      @click-left="$router.go(-1)"
+      fixed
+      placeholder
+    />
 
     <!-- 复选框 -->
-    <van-checkbox-group v-model="checked" ref="checkboxGroup" @change="handleChange">
-      <dl class="cart-container" v-for="(item, index) in cartData" :key="item.id + index">
+    <van-checkbox-group
+      v-model="checked"
+      ref="checkboxGroup"
+      @change="handleChange"
+    >
+      <dl
+        class="cart-container"
+        v-for="(item, index) in cartData"
+        :key="item.id + index"
+      >
         <dt>
-          <van-checkbox :name="item.id + '-' + item.color + '-' + item.size"></van-checkbox>
+          <van-checkbox
+            :name="item.id + '-' + item.color + '-' + item.size"
+          ></van-checkbox>
         </dt>
         <dd>
           <!-- 商品卡片 -->
-          <van-card :num="item.num" :price="item.price" :title="item.title"
-            :thumb="staticUrl + '/' + item.img">
+          <van-card
+            :num="item.num"
+            :price="item.price"
+            :title="item.title"
+            :thumb="staticUrl + '/' + item.img"
+          >
             <template #tags>
-              <van-tag v-if="item.color" plain type="danger">颜色:{{ item.color }}</van-tag>
-              <van-tag v-if="item.size" plain type="danger">尺寸{{ item.size }}</van-tag>
+              <van-tag v-if="item.color" plain type="danger"
+                >颜色:{{ item.color }}</van-tag
+              >
+              <van-tag v-if="item.size" plain type="danger"
+                >尺寸{{ item.size }}</van-tag
+              >
             </template>
             <template #footer>
               <van-button size="mini" @click="handleDec(index)">
@@ -32,12 +55,27 @@
     </van-checkbox-group>
 
     <!-- 订单提交 -->
-    <van-submit-bar class="cart-submit-bar" :price="totalPrice" button-text="提交订单" @submit="onSubmit">
-      <van-checkbox @click="handleCheckAll" v-model="checkAll">全选</van-checkbox>
-      <van-button @click="handleDel" size="mini" color="linear-gradient(to right, #ff6034, #ee0a24)"
-        style="margin-left:5px;">删除</van-button>
+    <van-submit-bar
+      class="cart-submit-bar"
+      :price="totalPrice"
+      button-text="提交订单"
+      @submit="onSubmit"
+    >
+      <van-checkbox @click="handleCheckAll" v-model="checkAll"
+        >全选</van-checkbox
+      >
+      <van-button
+        @click="handleDel"
+        size="mini"
+        color="linear-gradient(to right, #ff6034, #ee0a24)"
+        style="margin-left: 5px"
+        >删除</van-button
+      >
       <template #tip>
-        {{ selectAddressShow }} <van-button size="mini" type="default" @click="showCom = 'address'">选择地址</van-button>
+        {{ selectAddressShow }}
+        <van-button size="mini" type="default" @click="showCom = 'address'"
+          >选择地址</van-button
+        >
       </template>
     </van-submit-bar>
 
@@ -54,34 +92,39 @@
 
   <div v-show="showCom === 'order'" class="order">
     <!-- 确认订单组件 -->
-    <confirmOrder :address="selectAddress" :hideCom="hideCom" :selectPro="selectPro" :price="totalPrice"
-      :checked="checked" />
+    <confirmOrder
+      :address="selectAddress"
+      :hideCom="hideCom"
+      :selectPro="selectPro"
+      :price="totalPrice"
+      :checked="checked"
+    />
   </div>
 </template>
 
 <script>
-import { Notify } from "vant"
-import { mapGetters, mapActions } from 'vuex'
-import { staticUrl } from "../../api/api"
-import Recommend from "../../components/recommend.vue"
-import Address from "../../components/address.vue" // 引入地址组件
-import ConfirmOrder from "../../components/confirmOrder.vue" // 引入确认订单组件
+import { Notify } from "vant";
+import { mapGetters, mapActions } from "vuex";
+import { staticUrl } from "../../api/api";
+import Recommend from "../../components/recommend.vue";
+import Address from "../../components/address.vue"; // 引入地址组件
+import ConfirmOrder from "../../components/confirmOrder.vue"; // 引入确认订单组件
 export default {
-  name: 'Cart',
+  name: "Cart",
   components: {
     Recommend,
     myAddress: Address,
-    confirmOrder: ConfirmOrder
+    confirmOrder: ConfirmOrder,
   },
   data() {
     return {
       staticUrl,
       checked: [],
       checkAll: false,
-      showCom: 'cart', // 展示组件
+      showCom: "cart", // 展示组件
       showBack: this.$route.query.id, // 获取路径查询参数(?号后面的值)id的值
-      selectAddress: '', //用户选择的地址
-    }
+      selectAddress: "", //用户选择的地址
+    };
   },
 
   mounted() {
@@ -95,35 +138,44 @@ export default {
       if (this.getProfile.shoppingcart) {
         return JSON.parse(this.getProfile.shoppingcart);
       }
-      return []
+      return [];
     },
 
     // 购物车商品总价
     totalPrice() {
-      let count = 0
-      this.cartData.forEach(item => {
+      let count = 0;
+      this.cartData.forEach((item) => {
         // console.log(this)
-        if (this.checked.includes(item.id + '-' + item.color + '-' + item.size)) {
-          console.log(this.checked)
-          count += item.price * item.num
+        if (
+          this.checked.includes(item.id + "-" + item.color + "-" + item.size)
+        ) {
+          console.log(this.checked);
+          count += item.price * item.num;
         }
-      })
-      return count * 100
+      });
+      return count * 100;
     },
 
-    selectAddressShow() { // 计算地址显示内容
-      return this.selectAddress ? this.selectAddress.name + "," + this.selectAddress.tel : "您还没有设置收货地址"
+    selectAddressShow() {
+      // 计算地址显示内容
+      return this.selectAddress
+        ? this.selectAddress.name + "," + this.selectAddress.tel
+        : "您还没有设置收货地址";
     },
 
-    selectPro() { // 计算需要购买的商品数据 需要给确认订单组件传过去
-      let data = this.cartData.filter((item) => this.checked.includes(item.id + "-" + item.color + "-" + item.size));
+    selectPro() {
+      // 计算需要购买的商品数据 需要给确认订单组件传过去
+      let data = this.cartData.filter((item) =>
+        this.checked.includes(item.id + "-" + item.color + "-" + item.size)
+      );
       return data;
-    }
+    },
   },
 
   methods: {
-    ...mapActions('user', ['setProfileCartAction']),
-    handleChange() {  // 商品checkbox选择
+    ...mapActions("user", ["setProfileCartAction"]),
+    handleChange() {
+      // 商品checkbox选择
       // 每一次单选都要判断全选的状态是否正确。
       if (this.checked.length > 0 && this.checked.length === this.cartNum) {
         this.checkAll = true;
@@ -133,11 +185,14 @@ export default {
     },
 
     // 全选框的值变化要同步到单选框
-    handleCheckAll(value) { //通过判断所有商品是否都被选中来判断全选框的功能式全选还是反选
-      if(this.checked.length === this.cartNum){ //如果所有商品都被选中，那么就是反选
-        this.$refs.checkboxGroup.toggleAll(value)
-      }else{ //否则就是全选
-        this.$refs.checkboxGroup.toggleAll(true)
+    handleCheckAll(value) {
+      //通过判断所有商品是否都被选中来判断全选框的功能式全选还是反选
+      if (this.checked.length === this.cartNum) {
+        //如果所有商品都被选中，那么就是反选
+        this.$refs.checkboxGroup.toggleAll(value);
+      } else {
+        //否则就是全选
+        this.$refs.checkboxGroup.toggleAll(true);
       }
     },
 
@@ -147,7 +202,9 @@ export default {
       let cartData = this.cartData;
       for (let i = 0; i < cartData.length; i++) {
         let item = cartData[i];
-        if (this.checked.includes(item.id + "-" + item.color + "-" + item.size)) {
+        if (
+          this.checked.includes(item.id + "-" + item.color + "-" + item.size)
+        ) {
           cartData.splice(i, 1);
           i--;
         }
@@ -156,8 +213,8 @@ export default {
       // 删除以后需要把数据更新到后端和全局状态
       this.setProfileCartAction({
         profile: this.getProfile,
-        cartData
-      })
+        cartData,
+      });
     },
 
     // 商品数量增加
@@ -166,8 +223,8 @@ export default {
       cartData[index].num++;
       this.setProfileCartAction({
         profile: this.getProfile,
-        cartData
-      })
+        cartData,
+      });
     },
 
     // 商品数量减少
@@ -177,11 +234,12 @@ export default {
       cartData[index].num--;
       this.setProfileCartAction({
         profile: this.getProfile,
-        cartData
-      })
+        cartData,
+      });
     },
 
-    onSubmit() { // 提交订单
+    onSubmit() {
+      // 提交订单
       if (this.checked.length === 0) {
         Notify("请至少选择一款购买商品");
         return;
@@ -190,19 +248,19 @@ export default {
         Notify("请选择收货地址");
         return;
       }
-      this.showCom = 'order'
+      this.showCom = "order";
     },
 
-    hideCom(type) { // 隐藏地址组件
-      this.showCom = type
+    hideCom(type) {
+      // 隐藏地址组件
+      this.showCom = type;
     },
-    handleAddress(address) { // 子组件传递选中的地址到该方法
+    handleAddress(address) {
+      // 子组件传递选中的地址到该方法
       this.selectAddress = address;
-    }
-
-
-  }
-}
+    },
+  },
+};
 </script>
 
 <style lang="scss">
@@ -231,13 +289,13 @@ export default {
     background-color: #fff;
   }
 
-  &>dt {
+  & > dt {
     display: flex;
     width: 0.67rem;
     justify-content: center;
   }
 
-  &>dd {
+  & > dd {
     flex: 1;
     margin: 0;
   }
